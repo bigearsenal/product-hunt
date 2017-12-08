908edea11dab1f520eddb0fcd25074f87b8588ac
Chung Tran <bigearsenal@gmail.com>
2017-12-08 17:54:43 +0300

    Remove default font

--- producthunt/AppDelegate.swift
+++ producthunt/AppDelegate.swift
@@ -17,9 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        // Change default font to Avernir Next.
-        UILabel.appearance().font = UIFont(name: "Avenir Next", size: 17);
-        
         return true
     }
 
